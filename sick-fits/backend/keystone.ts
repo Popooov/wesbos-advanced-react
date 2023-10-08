502604a366206e1703859523348f632963e135e8
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2023-10-08 21:35:22 +0000

    Create keystone.ts

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,32 @@
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import "dotenv/config";
+
+const databaseURL =
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: "mongoose",
+    url: databaseURL,
+    // todo
+  },
+  lists: createSchema({
+    // schema
+  }),
+  ui: {
+    // todo
+    isAccessAllowed: () => true,
+  },
+  // todo
+});
