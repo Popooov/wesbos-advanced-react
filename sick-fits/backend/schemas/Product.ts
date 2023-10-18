70e278890e00a7e13dde0aa194b908a85b964ea6
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2023-10-18 20:30:41 +0000

    feat: Added relationship field to Product and ProductImage schemas
    Added 'photo' relationship field to Product schema and 'product' relationship field to ProductImage schema

--- sick-fits/backend/schemas/Product.ts
+++ sick-fits/backend/schemas/Product.ts
@@ -1,4 +1,4 @@
-import { integer, select, text } from "@keystone-next/fields";
+import { integer, relationship, select, text } from "@keystone-next/fields";
 import { list } from "@keystone-next/keystone/schema";
 
 export const Product = list({
@@ -10,6 +10,7 @@ export const Product = list({
         displayMode: "textarea",
       },
     }),
+    photo: relationship({ ref: 'ProductImage.product' }),
     status: select({
       options: [
         { label: "Draft", value: "DRAFT" },
