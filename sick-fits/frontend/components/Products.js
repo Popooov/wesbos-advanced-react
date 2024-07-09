c9e18069b75360af0b74ed8514b5582bda2b68da
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-07-09 09:07:04 +0000

    style: Refactor Products.js to use styled-components for better styling

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -1,5 +1,6 @@
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/client";
+import styled from "styled-components";
 
 const ALL_PRODUCTS_QUERY = gql`
   query ALL_PRODUCTS_QUERY {
@@ -18,6 +19,12 @@ const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
+const ProductsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
 export default function Products() {
   const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
   console.log(data, error, loading);
@@ -25,11 +32,11 @@ export default function Products() {
   if (error) return <p>Error: {error.message}</p>;
   return (
     <div>
-      <div>
+      <ProductsListStyles>
         {data.allProducts.map((product) => (
           <p key={product.id}>{product.name}</p>
         ))}
-      </div>
+      </ProductsListStyles>
     </div>
   );
 }
