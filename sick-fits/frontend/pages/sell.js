b2ff5738482cfb92c548f6a27148497264763543
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-07-10 23:04:21 +0000

    feat: Add CreateProduct component to SellPage

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,3 +1,9 @@
-export default function SellPage(params) {
-  return <p>This is Sell Page</p>;
+import CreateProduct from "../components/CreateProduct";
+
+export default function SellPage() {
+  return (
+    <div>
+      <CreateProduct />
+    </div>
+  );
 }
