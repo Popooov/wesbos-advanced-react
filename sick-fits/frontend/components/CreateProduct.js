977bd398a4db771134d0f85d9f2189d026a1411e
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-07-21 22:27:34 +0000

    feat: Add CREATE_PRODUCT_MUTATION

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,5 +1,30 @@
+import gql from "graphql-tag";
 import useForm from "../lib/useForm";
 import Form from "./styles/Form";
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      name
+      price
+      description
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
